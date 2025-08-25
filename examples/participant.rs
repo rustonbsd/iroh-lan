911f95d656b592eb1179c8a6fc939db79663a752
rustonbsd <rustonbsd@mailfence.com>
2025-08-25 22:10:57 +0200

    sync

---
+++ examples/participant.rs
@@ -0,0 +1,34 @@
+use std::{io::Read, time::Duration};
+
+use iroh::{NodeId, SecretKey};
+use tokio::{signal::ctrl_c, time::sleep};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let secret = SecretKey::generate(&mut rand::thread_rng());
+
+    let mut router = iroh_lan::Router::builder()
+        .entry_name("my-lan-party")
+        .secret_key(secret)
+        .build()
+        .await?;
+
+        tokio::spawn({
+            let mut router = router.clone();
+            async move {
+                let mut rec = router.gossip_receiver.subscribe().await.unwrap();
+                loop {
+                    println!("msg: {:?}", rec.recv().await);
+                }
+            }
+        });
+    
+
+    loop {
+        println!("{:?}",router.get_state().await);
+        sleep(Duration::from_secs(5)).await;
+    }
+
+    ctrl_c().await.unwrap();
+    Ok(())
+}
--- src/router.rs
+++ src/router.rs
@@ -1,36 +1,51 @@
-use std::{collections::HashMap, net::Ipv4Addr, sync::Arc};
+use std::{collections::HashMap, net::Ipv4Addr, time::{Duration, SystemTime}};
 
-use distributed_topic_tracker::{AutoDiscoveryGossip, GossipReceiver, GossipSender, TopicId};
-use iroh::{
-    NodeId,
-    protocol::{AcceptError, ProtocolHandler},
-};
+use distributed_topic_tracker::{AutoDiscoveryGossip, GossipReceiver, GossipSender, Topic, TopicId};
+use iroh::NodeId;
 use serde::{Deserialize, Serialize};
-use tokio::io::AsyncReadExt;
 
-use anyhow::Result;
+use anyhow::{Result, bail};
 use distributed_topic_tracker::{AutoDiscoveryBuilder, DefaultSecretRotation};
 use iroh::{Endpoint, SecretKey};
-use iroh_gossip::net::Gossip;
+use iroh_gossip::{
+    net::Gossip,
+    proto::HyparviewConfig,
+};
+use tokio::time::sleep;
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct Router {
-    gossip_sender: GossipSender,
-    gossip_receiver: GossipReceiver,
+    pub gossip_sender: GossipSender,
+    pub gossip_receiver: GossipReceiver,
     router_requester: tokio::sync::broadcast::Sender<RouterRequest>,
+    keep_alive_router_requester: tokio::sync::broadcast::Receiver<RouterRequest>,
+    pub node_id: NodeId,
+    _topic: Option<Topic<DefaultSecretRotation>>,
+}
+
+impl Clone for Router {
+    fn clone(&self) -> Self {
+        Self {
+            gossip_sender: self.gossip_sender.clone(),
+            gossip_receiver: self.gossip_receiver.clone(),
+            router_requester: self.router_requester.clone(),
+            keep_alive_router_requester: self.router_requester.subscribe(),
+            node_id: self.node_id.clone(),
+            _topic: None,
+        }
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct StateMessage {
     node_id_ip_dict: HashMap<NodeId, Ipv4Addr>,
     timestamp: i64,
-    leader: NodeId,
+    leader: Option<NodeId>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ReqMessage{
+pub struct ReqMessage {
     node_id: NodeId,
-    ipv4: Ipv4Addr,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -39,19 +54,19 @@ enum RouterMessage {
     ReqMessage(ReqMessage),
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 enum RouterRequest {
-    ReqGetRouterState(tokio::sync::broadcast::Sender<RouterState>),
-    ReqSetNodeIdIpDict(
+    GetRouterState(tokio::sync::oneshot::Sender<RouterState>),
+    SetNodeIdIpDict(
         HashMap<NodeId, Ipv4Addr>,
-        tokio::sync::broadcast::Sender<()>,
+        tokio::sync::oneshot::Sender<()>,
     ),
-    ReqSetLeader(NodeId, tokio::sync::broadcast::Sender<()>),
-    ReqSetLastLeaderMsg(StateMessage, tokio::sync::broadcast::Sender<()>),
+    SetLeader(NodeId, tokio::sync::oneshot::Sender<()>),
+    SetLastLeaderMsg(StateMessage, tokio::sync::oneshot::Sender<()>),
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-struct RouterState {
+pub struct RouterState {
     node_id_ip_dict: HashMap<NodeId, Ipv4Addr>,
     leader: Option<NodeId>,
     last_leader_msg: Option<StateMessage>,
@@ -61,8 +76,7 @@ struct RouterState {
 pub struct Builder {
     entry_name: String,
     secret_key: SecretKey,
-
-    // FIX UP BUILDER AND MAKE GOSSIP WORK WITH NODEID_IPV4 pairs
+    creator_mode: bool,
 }
 
 impl Builder {
@@ -70,8 +84,19 @@ impl Builder {
         Builder::default()
     }
 
-    pub fn node_id_ip(&mut self, node_id: NodeId, ip: Ipv4Addr) -> Self {
-        self.node
+    pub fn entry_name(mut self, entry_name: &str) -> Self {
+        self.entry_name = entry_name.to_string();
+        self
+    }
+
+    pub fn secret_key(mut self, secret_key: SecretKey) -> Self {
+        self.secret_key = secret_key;
+        self
+    }
+
+    pub fn creator_mode(mut self) -> Self {
+        self.creator_mode = true;
+        self
     }
 
     pub async fn build(&self) -> Result<Router> {
@@ -80,32 +105,72 @@ impl Builder {
             .secret_key(self.secret_key.clone())
             .bind()
             .await?;
+        println!("1");
+
+        let gossip_config = HyparviewConfig {
+            neighbor_request_timeout: Duration::from_millis(2000),
+            ..Default::default()
+        };
+
         let gossip = Gossip::builder()
-            .spawn_with_auto_discovery(endpoint.clone(), Some(DefaultSecretRotation))
+            .membership_config(gossip_config)
+            .spawn_with_auto_discovery::<DefaultSecretRotation>(endpoint.clone(), None)
             .await?;
+        println!("2");
         let _router = iroh::protocol::Router::builder(endpoint.clone())
-            .accept(iroh_gossip::ALPN, gossip.gossip.clone());
+            .accept(iroh_gossip::ALPN, gossip.gossip.clone())
+            .spawn();
+        println!("3");
 
         let topic_initials = format!("lanparty-{}", self.entry_name);
         let secret_initials = format!("{topic_initials}-secret").as_bytes().to_vec();
-        let (gossip_sender, gossip_receiver) = gossip
-            .subscribe_and_join_with_auto_discovery(TopicId::new(topic_initials), &secret_initials)
-            .await?
-            .split();
 
-        let (router_state_sender, mut router_state_reader) = tokio::sync::broadcast::channel(1024);
-        tokio::spawn(async move { while router_state_reader.recv().await.is_ok() {} });
+        println!("4");
+        let topic = if self.creator_mode {
+            gossip
+                .subscribe_and_join_with_auto_discovery(
+                    TopicId::new(topic_initials),
+                    secret_initials,
+                )
+                .await?
+        } else {
+            gossip
+                .subscribe_and_join_with_auto_discovery(
+                    TopicId::new(topic_initials),
+                    secret_initials,
+                )
+                .await?
+        };
 
+        let (gossip_sender, gossip_receiver) = topic.split();
+
+        println!("5");
+
+        let (router_state_sender,  router_state_reader) = tokio::sync::mpsc::channel(1024);
+
+
+        let router_state_hs = if self.creator_mode {
+            let mut hs = HashMap::<NodeId, Ipv4Addr>::new();
+            hs.insert(endpoint.node_id(), Ipv4Addr::new(172, 22, 0, 2));
+            hs
+        } else {
+            HashMap::<NodeId, Ipv4Addr>::new()
+        };
         let mut router_state = RouterState {
-            node_id_ip_dict: HashMap::<NodeId, Ipv4Addr>::new(),
-            leader: None,
+            node_id_ip_dict: router_state_hs,
+            leader: if self.creator_mode {
+                Some(endpoint.node_id())
+            } else {
+                None
+            },
             last_leader_msg: None,
         };
 
+        println!("7");
+
         tokio::spawn({
-            let sender = router_state_sender.clone();
             async move {
-                router_state.spawn(sender.clone()).await;
+                router_state.spawn(router_state_reader).await;
             }
         });
 
@@ -113,7 +178,11 @@ impl Builder {
             gossip_sender,
             gossip_receiver,
             router_requester: router_state_sender,
+            keep_alive_router_requester: keep_alive_router_state_sender,
+            node_id: endpoint.node_id(),
+            _topic: Some(topic),
         };
+
 
         tokio::spawn({
             let router = router.clone();
@@ -122,6 +191,20 @@ impl Builder {
             }
         });
 
+
+        println!("8");
+        if !self.creator_mode {
+            sleep(Duration::from_secs(5)).await;
+            println!("1");
+            let data = serde_json::to_vec(&RouterMessage::ReqMessage(ReqMessage {
+                node_id: endpoint.node_id(),
+            }))?;
+            println!("1");
+            router.gossip_sender.broadcast(data).await?;
+            println!("1");
+        }
+        println!("finished:");
+
         Ok(router)
     }
 }
@@ -129,6 +212,7 @@ impl Builder {
 impl Default for Builder {
     fn default() -> Self {
         Self {
+            creator_mode: false,
             entry_name: String::default(),
             secret_key: SecretKey::generate(&mut rand::thread_rng()),
         }
@@ -141,7 +225,8 @@ impl Router {
     }
 
     async fn spawn(&self) -> Result<()> {
-        let mut recv = self.gossip_receiver.clone();
+        println!("router.spawn");
+        let mut recv = self.gossip_receiver.clone().subscribe().await?;
         while let Ok(event) = recv.recv().await {
             if let iroh_gossip::api::Event::Received(message) = event {
                 if let Ok(router_msg) =
@@ -152,41 +237,107 @@ impl Router {
                         RouterMessage::StateMessage(state_message) => {
                             if let Ok(state) = self.get_state().await {
                                 let _ = self.set_last_leader_msg(state_message.clone()).await;
+                                let _ = self
+                                    .set_node_id_ip_dict(state_message.node_id_ip_dict)
+                                    .await;
                                 if state.leader.is_none()
-                                    || state.leader.expect("") != state_message.leader
+                                    || (state_message.leader.is_some() && state.leader.unwrap() != state_message.leader.unwrap())
                                 {
-                                    let _ = self.set_leader(state_message.leader).await;
+                                    let _ = self.set_leader(state_message.leader.unwrap()).await;
                                 }
                             }
                         }
                         RouterMessage::ReqMessage(req_message) => {
-                            if self.add_node_id_ip(req_message.node_id, req_message.ipv4).await.is_ok() {
-                                if let Ok(state) = self.get_state().await {
-                                    let data = serde_json::to_vec(&state).expect("serialization failed");
-                                    let _ = self.gossip_sender.broadcast(data).await;
-                                    println!("123");
+                            println!("start: req_message {req_message:?}");
+                            if let Ok(state) = self.get_state().await {
+                                if let Some(leader) = state.leader {
+                                    if leader == self.node_id {
+                                        if let Ok(next_ip) = self.get_next_ip().await {
+                                            if self
+                                                .add_node_id_ip(req_message.node_id, next_ip)
+                                                .await
+                                                .is_ok()
+                                            {
+                                                if let Ok(state) = self.get_state().await {
+                                                    let msg = RouterMessage::StateMessage(StateMessage { node_id_ip_dict: state.node_id_ip_dict, timestamp: SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap_or(Duration::from_secs(0)).as_secs() as i64, leader: state.leader });
+                                                    let data = serde_json::to_vec(&msg)
+                                                        .expect("serialization failed");
+                                                    let _ =
+                                                        self.gossip_sender.broadcast(data).await;
+
+                                                    println!("end: req_message");
+                                                }
+                                            }
+                                        } else {
+                                            println!("get next ip failed");
+                                        }
+                                    }
                                 }
                             }
-                        },
+                        }
                     }
+                } else {
+                    println!("Failed to deserialize gossip message");
                 }
+
             }
         }
+
+        println!("Failed!!!!");
         Ok(())
     }
 
+    async fn get_next_ip(&self) -> Result<Ipv4Addr> {
+        println!("a");
+        let state = self.get_state().await?;
+        println!("b");
+        let &highest_ip = state
+            .node_id_ip_dict
+            .values()
+            .max_by_key(|&ip| ip.octets()[2] as u16 * 256u16 + ip.octets()[3] as u16)
+            .unwrap_or(&Ipv4Addr::new(172, 22, 0, 3));
+
+        println!("c");
+        let next_ip = Ipv4Addr::new(
+            172,
+            22,
+            if highest_ip.octets()[3] == 255 { 1 } else { 0 } + highest_ip.octets()[2],
+            if highest_ip.octets()[3] == 255 {
+                0
+            } else {
+                highest_ip.octets()[3]+1
+            },
+        );
+
+        println!("d: {next_ip}");
+        // to avoid overflow and therfore dublicates we check if this ip is already contained
+        if state.node_id_ip_dict.values().any(|v| v.eq(&next_ip)) {
+            println!("e");
+            bail!("invalid ip")
+        }
+
+        println!("f");
+        Ok(next_ip)
+    }
+
     async fn add_node_id_ip(&self, node_id: NodeId, ip: Ipv4Addr) -> Result<()> {
+        println!("1");
         let mut state = self.get_state().await?;
+        println!("2");
         let _ = state.node_id_ip_dict.insert(node_id, ip);
+        println!("3");
         self.set_node_id_ip_dict(state.node_id_ip_dict).await?;
+        println!("4");
 
         Ok(())
     }
 
     async fn set_last_leader_msg(&self, latest_leader_msg: StateMessage) -> Result<()> {
         let (tx, mut rx) = tokio::sync::broadcast::channel(1);
-        self.router_requester
-            .send(RouterRequest::ReqSetLastLeaderMsg(latest_leader_msg, tx))?;
+        self.router_requester.send(RouterRequest::SetLastLeaderMsg(
+            latest_leader_msg,
+            tx.clone(),
+        ))?;
         rx.recv().await?;
         Ok(())
     }
@@ -194,7 +345,7 @@ impl Router {
     async fn set_leader(&self, leader: NodeId) -> Result<()> {
         let (tx, mut rx) = tokio::sync::broadcast::channel(1);
         self.router_requester
-            .send(RouterRequest::ReqSetLeader(leader, tx))?;
+            .send(RouterRequest::SetLeader(leader, tx.clone()))?;
         rx.recv().await?;
         Ok(())
     }
@@ -202,37 +353,37 @@ impl Router {
     async fn set_node_id_ip_dict(&self, node_id_ip_dict: HashMap<NodeId, Ipv4Addr>) -> Result<()> {
         let (tx, mut rx) = tokio::sync::broadcast::channel(1);
         self.router_requester
-            .send(RouterRequest::ReqSetNodeIdIpDict(node_id_ip_dict, tx))?;
+            .send(RouterRequest::SetNodeIdIpDict(node_id_ip_dict, tx.clone()))?;
         rx.recv().await?;
         Ok(())
     }
 
-    async fn get_state(&self) -> Result<RouterState> {
+    pub async fn get_state(&self) -> Result<RouterState> {
         let (tx, mut rx) = tokio::sync::broadcast::channel(1);
         self.router_requester
-            .send(RouterRequest::ReqGetRouterState(tx))?;
+            .send(RouterRequest::GetRouterState(tx.clone()))?;
         Ok(rx.recv().await?)
     }
 }
 
 impl RouterState {
-    async fn spawn(&mut self, sender: tokio::sync::broadcast::Sender<RouterRequest>) {
-        let mut reader = sender.subscribe();
+    async fn spawn(&mut self,reader: tokio::sync::mpsc::Receiver<RouterRequest>) {
+        let mut reader = reader;
         loop {
             tokio::select! {
-                Ok(router_request) = reader.recv() => match router_request {
-                    RouterRequest::ReqGetRouterState(sender)=>{
+                Some(router_request) = reader.recv() => match router_request {
+                    RouterRequest::GetRouterState(sender)=>{
                         let _ = sender.send(self.clone());
                     },
-                    RouterRequest::ReqSetNodeIdIpDict(hash_map, sender) => {
+                    RouterRequest::SetNodeIdIpDict(hash_map, sender) => {
                         self.node_id_ip_dict = hash_map;
                         let _ = sender.send(());
                     },
-                    RouterRequest::ReqSetLeader(public_key, sender) => {
+                    RouterRequest::SetLeader(public_key, sender) => {
                         self.leader = Some(public_key);
                         let _ = sender.send(());
                     },
-                    RouterRequest::ReqSetLastLeaderMsg(router_message, sender) => {
+                    RouterRequest::SetLastLeaderMsg(router_message, sender) => {
                         self.last_leader_msg = Some(router_message);
                         let _ = sender.send(());
                     },
